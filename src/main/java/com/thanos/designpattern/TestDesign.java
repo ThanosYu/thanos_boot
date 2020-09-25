2704bf7599c2dc8b282163f650689ceb4de27919
Yu Shi Qiang <shiqiang.yu@siemens.com>
2020-09-25 16:53:52 +0800

    factory pattern
    https://www.runoob.com/design-pattern/factory-pattern.html

---
+++ src/main/java/com/thanos/designpattern/TestDesign.java
@@ -0,0 +1,8 @@
+package com.thanos.designpattern;
+
+/**
+ * @author Shi Qiang Yu
+ * @date 9/25/2020 4:32 PM
+ */
+public class TestDesign {
+}
---
+++ src/main/java/com/thanos/designpattern/factory/Shape.java
@@ -0,0 +1,9 @@
+package com.thanos.designpattern.factory;
+
+/**
+ * @author Shi Qiang Yu
+ * @date 9/25/2020 4:33 PM
+ */
+public interface Shape {
+    void draw();
+}
