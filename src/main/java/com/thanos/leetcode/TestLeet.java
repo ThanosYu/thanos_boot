292cd766c061a2dd2e602333874cff7ccfb1b8c3
Administrator <15901878370@163.com>
2020-11-02 21:52:32 +0800

    node tree

--- src/main/java/com/thanos/leetcode/TestLeet.java
+++ src/main/java/com/thanos/leetcode/TestLeet.java
@@ -1,48 +1,27 @@
 package com.thanos.leetcode;
 
 
-import java.util.Arrays;
-
 /**
  * @author Shi Qiang Yu
  * @date 4/8/2020 9:19 PM
  */
 public class TestLeet {
 
-    public String[] findRelativeRanks(int[] nums) {
-        int[] tempArray = nums.clone();
-        for (int i = 0; i < tempArray.length - 1; i++) {
-            for (int j = 0; j < tempArray.length - 1 - i; j++) {
-                if (tempArray[j] < tempArray[j + 1]) {
-                    int tmp = tempArray[j];
-                    tempArray[j] = tempArray[j + 1];
-                    tempArray[j + 1] = tmp;
-                }
-            }
+    public TreeNode invertTree(TreeNode root) {
+        if (null == root) {
+            return null;
         }
-        System.out.println(Arrays.toString(tempArray));
-        String[] res = new String[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == tempArray[0]) {
-                res[i] = "Gold Medal";
-            } else if (nums[i] == tempArray[1]) {
-                res[i] = "Silver Medal";
-            } else if (nums[i] == tempArray[2]) {
-                res[i] = "Bronze Medal";
-            } else {
-                for (int j = 3; j < tempArray.length; j++) {
-                    if (nums[i] == tempArray[j]) {
-                        res[i] = String.valueOf(j + 1);
-                    }
-                }
-            }
-        }
-        return res;
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = invertTree(right);
+        root.right = invertTree(left);
+        return root;
     }
 
     public static void main(String[] args) {
         TestLeet testLeet = new TestLeet();
-        System.out.println((Arrays.toString(testLeet.findRelativeRanks(new int[]{9, 2, 6, 5, 7}))));
+        TreeNode treeNode = new TreeNode(9);
+        System.out.println("=====result: " + testLeet.invertTree(treeNode));
     }
 }
 
